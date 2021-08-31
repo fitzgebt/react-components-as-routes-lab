60816b04e9c838d65eac66e2251cce2eacd40c5f
fitzgebt <fitzgebt@gmail.com>
2021-08-31 12:15:46 -0700

    commit tree 5886f480fa018bf00e7d30736e44e21668015802

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,21 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((actor, index) => {
+        return (
+          <div key={index}>
+            <p>{actor.name}</p>
+            <ul>
+              {actor.movies.map((movie, index) => {
+                return (
+                  <li key={index}>{movie}</li>
+                )
+              })}
+            </ul>
+          </div>
+        )
+      })}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -3,8 +3,22 @@ import { movies } from '../data';
 
 const Movies = () => {
   return (
-    <div>
-        {/*{code here}*/}
+    <div className='moviesDiv'>
+        <h1>Movies Page</h1>
+        {movies.map((movie, index) => {
+          return (
+            <div key={index}>
+            <p>{movie.title} {movie.time}</p>
+            <ul>
+              {movie.genres.map((genre, index) => {
+                return (
+                  <li key={index}>{genre}</li>
+                  )
+              })}
+            </ul>
+            </div>
+          )
+        })}
     </div>
   );
 };
